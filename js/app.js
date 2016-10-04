6c96cad5e521e23e7c1bda191a532d5e002c0864
Gluhouse <Artem66691@gmail.com>
2016-10-04 15:08:18 +0300

    TESTv1

--- js/app.js
+++ js/app.js
@@ -6,5 +6,39 @@ $(document).ready(function() {
   .mouseleave(function() {
     $('.ryu-ready').hide();
 $('.ryu-still').show();
-  });
+  })
+  .mousedown(function() {
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show().animate({'left':'1020px'},500,function() {
+      $('.hadouken').hide();
+      $('.hadouken').css('left', '520px');
+    }
+  );
+  })
+  .mouseup(function(){
+    $('.ryu-throwing').hide();
+  $('.ryu-ready').show();
 });
+$('.main').keydown(function(){
+  alert('Test');
+  $('ryu-still').hide();
+  $('ryu-cool').show();
+});
+});
+$(document).ready(function(){
+  playMusic();
+});
+
+function playMusic () {
+  $('#music')[0].volume = 0.5;
+  $('#music')[0].load();
+  $('#music')[0].play();
+}
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
